6c177d022118e43aa20f73d17832a57cd1e228be
Valentin Vasilyev <valentin.vasilyev@outlook.com>
2017-02-09 11:13:25 -0600

    Remove deprecation warnings and did some refactoring

--- lib/tsc-ruby.rb
+++ lib/tsc-ruby.rb
@@ -29,18 +29,18 @@ module TypeScript
       # @return [String] compiled JavaScript
       def compile(script, *tsc_options)
         script = script.read if script.respond_to?(:read)
-        js_file = Tempfile.new(%w(tsc-ruby .ts))
+        input_file = Tempfile.new(%w(tsc-ruby .ts))
         begin
-          js_file.write(script)
-          js_file.close
-          result = compile_file(js_file.path, *tsc_options)
+          input_file.write(script)
+          input_file.close
+          result = compile_file(input_file.path, *tsc_options)
           if result.success?
             result.js
           else
             raise result.stderr + result.stdout
           end
         ensure
-          js_file.unlink
+          input_file.unlink
         end
       end
 
@@ -50,11 +50,13 @@ module TypeScript
       #
       # @param [String] TypeScript script file
       # @return [TypeScript::Ruby::CompileResult] compile result
-      def compile_file(source_file, *tsc_options)
+      def compile_file(input_file_path, *tsc_options)
         Dir.mktmpdir do |output_dir|
-          stdout, stderr, exit_status = tsc(*tsc_options, '--outDir', output_dir, source_file)
-          output_file = "#{File.join(output_dir, File.basename(source_file, '.ts'))}.js"
-          output_js = File.exist?(output_file) ? File.read(output_file) : nil
+          stdout, stderr, exit_status = tsc(*tsc_options, '--outDir', output_dir, input_file_path)
+          # now input file is in output_dir but with a .js extension
+          output_file_path = File.join(output_dir, File.basename(input_file_path, '.ts')) + '.js'
+          raise "Compiled JS file not found: #{output_file_path}" unless File.exist? output_file_path
+          output_js = File.read(output_file_path)
           CompileResult.new(output_js, exit_status, stdout, stderr)
         end
       end
